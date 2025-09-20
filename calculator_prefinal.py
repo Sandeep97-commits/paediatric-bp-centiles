b5679be4fc8829c1b4aba34a25cb85b45e7aaf2b
Sandeep97-commits <sandeep.3856@aiims.edu>
2025-09-20 14:39:28 +0530

    Update calculator_prefinal.py

--- calculator_prefinal.py
+++ calculator_prefinal.py
@@ -273,8 +273,12 @@ if height_results:
             unsafe_allow_html=True,
         )
 
-        st.markdown("------")
-        st.markdown(
-            "**References:** [AAP Clinical Practice Guideline for Screening and Diagnosis](https://publications.aap.org/pediatrics/article/140/3/e20171904/38358/Clinical-Practice-Guideline-for-Screening-and?autologincheck=redirected)",
-            unsafe_allow_html=True
-        )
+         # ---------------- References (moved outside loop, at bottom) ----------------
+       st.markdown("----")  # longer separator to push it down visually
+       st.subheader("References")
+       st.markdown(
+       """
+       - [AAP Clinical Practice Guideline for Screening and Diagnosis](https://publications.aap.org/pediatrics/article/140/3/e20171904/38358/Clinical-Practice-Guideline-for-Screening-and?autologincheck=redirected)  
+       - [PALS 2024 Algorithm](https://www.acls-pals-bls.com/algorithms/pals/)
+       """
+       )
