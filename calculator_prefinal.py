7412f27ea53e4e9c2bf7f752642d8a4fe78ffdf5
Sandeep97-commits <sandeep.3856@aiims.edu>
2025-09-20 16:16:22 +0530

    Update calculator_prefinal.py

--- calculator_prefinal.py
+++ calculator_prefinal.py
@@ -62,16 +62,19 @@ def create_pdf(age, gender, height, sbp, dbp, map_val,
     pdf.ln(5)
 
     # Centile notes (with adjustment note)
-    pdf.set_font("Arial", 'B', 12)  # bold for heading
-    pdf.cell(20, 10, "SBP:", ln=0)  # print SBP label
-    pdf.set_font("Arial", size=12)  # normal for value
+    pdf.set_font("Arial", 'B', 12)
+    pdf.multi_cell(0, 10, "SBP:", align="L")
+
+    pdf.set_font("Arial", size=12)
     pdf.multi_cell(0, 10, f"{sbp_result} (adjusted for height, age and gender).", align="L")
+
     # DBP
-    pdf.set_font("Arial", 'B', 12)  # bold for heading
-    pdf.cell(20, 10, "DBP:", ln=0)  # print DBP label
-    pdf.set_font("Arial", size=12)  # normal for value
+    pdf.set_font("Arial", 'B', 12)
+    pdf.multi_cell(0, 10, "DBP:", align="L")
+
+    pdf.set_font("Arial", size=12)
     pdf.multi_cell(0, 10, f"{dbp_result} (adjusted for height, age and gender).", align="L")
-    pdf.ln(10)
+
 
     # References
     pdf.set_font("Arial", 'B', 14)
